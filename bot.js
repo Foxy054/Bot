0a78d1e802183cfdb3237d87b0679698c12c9353
Foxy054 <67273963+Foxy054@users.noreply.github.com>
2020-06-22 17:39:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,13 +28,13 @@ bot.on('message', message => {
   var fact = Math.floor(Math.random() * facts.length);
   message.channel.send(facts[fact]);
   }
-  else if(message.content == "or") {
+ else if(message.content.includes("or") {
   message.channel.send("Or What?");
   }
-  else if(message.content == "madison") {
+  else if(message.content.includes("madison") {
   message.channel.send("Nice Name");
   }
-  else if(message.content == "fox") {
+  else if(message.content.includes("fox") {
   message.channel.send("Where Fox?!? I Wanna Pet Him");
   }
   else if(message.content == "wolfie") {
@@ -43,41 +43,38 @@ bot.on('message', message => {
   else if(message.content == "שלום") {
   message.channel.send("Translation Please... Alien");
   }
-  else if(message.content == "me big brain") {
+  else if(message.content.includes("me big brain") {
   message.channel.send("No You Small Brain Foxy, Im The Big Brain!");
   }
-  else if(message.content == "later") {
+  else if(message.content.includes("later") {
   message.channel.send("There Is No Later Its Today!!!");
   }
-  else if(message.content == "beat") {
+  else if(message.content.includes("beat") {
   message.channel.send("Stop Scorpio Bad No Beating Foxy");
   }
-  else if(message.content == "minecraft") {
+  else if(message.content.includes("minecraft") {
   message.channel.send("Is A Nice Game");
   }
-  else if(message.content == "youtube") {
+  else if(message.content.includes("youtube") {
   message.channel.send("HEHE Scorpio Leaving Foxy");
   }
-  else if(message.content == "Foxy") {
+  else if(message.content.includes("foxy") {
   message.channel.send("Foxy Is Probably Crying In An Allyway Or Something, I Dont Care!");
   }
-  else if(message.content == "scorpio") {
+  else if(message.content.includes("scorpio") {
   message.channel.send("Madison Is Cool");
   }
-  else if(message.content == "crush") {
+  else if(message.content.includes("crush") {
   message.channel.send("Someone Has A Crush On Somebody HEHE");
   }
-  else if(message.content == "meme") {
+  else if(message.content.includes("meme") {
   message.channel.send("Foxys Memes Are So Bad I Puked On My Razer BlackWidow, Fuck You Foxy!");
   }
-  else if(message.content == "cute") {
+  else if(message.content.includes("cute") {
   message.channel.send("Who You Callin Cute?");
   }
-  else if(message.content == "foxes") {
+  else if(message.content.includes("foxes") {
   message.channel.send("Foxes Are Cool I Need One Right Now!");
-  }
-  else if(message.content == "Someone Has A Crush On Somebody HEHE") {
-  message.channel.send("--ship Foxy Scorpio");
   }
 });
 
